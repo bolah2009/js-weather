da718f89889a78568809bcffcd8cf37a8bec6aa2
Bola Ahmed Buari <bola_ahmed2002@yahoo.com>
2019-10-20 15:55:23 +0100

    Format data and add unit conversions

---
+++ src/modules/helpers/date.js
@@ -0,0 +1,41 @@
+const formatDate = () => {
+  const now = new Date();
+
+  const week = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+
+  const newDate = date => new Date(date);
+  const weekIndex = date => newDate(date).getDay();
+  const dayOfTheWeek = date => week[weekIndex(date)];
+  const isFuture = date => newDate(date) > now;
+
+  const toTime = (date = now) => {
+    const hour = newDate(date).getHours();
+    const minutes = newDate(date).getMinutes();
+    const timePeriods = hour < 12 ? 'AM' : 'PM';
+    const formatMinutes = minutes < 10 ? `0${minutes}` : minutes;
+    const formatHour = () => {
+      const newHour = hour > 12 ? hour - 12 : hour;
+      if (newHour < 12) { return `0${newHour}`; }
+      return newHour;
+    };
+    return `${formatHour()}:${formatMinutes} ${timePeriods}`;
+  };
+
+  const toFullDay = date => dayOfTheWeek(date);
+  const toShortDay = (date, take = 3) => dayOfTheWeek(date).slice(0, take);
+  const toFullDayWithTime = date => `${dayOfTheWeek(date)} (${toTime(date)})`;
+
+  return {
+    toFullDay, toShortDay, toFullDayWithTime, isFuture,
+  };
+};
+
+export default formatDate;
--- src/modules/weather.js
+++ src/modules/weather.js
@@ -1,51 +1,23 @@
+import formatData from './helpers/formatUnits';
+
 const apiKey = process.env.WEATHER_API;
 
-const getWeatherData = async (location) => {
-  const uri = `https://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=${apiKey}&units=metric`;
+const fetchWeatherData = async city => {
+  const uri = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
   const response = await fetch(uri);
-  const { list, city: { name: cityName } } = await response.json();
+  const data = await response.json();
+  const { ok } = response;
+  return { data, ok };
+};
 
-  const getForcastData = (data) => {
-    const {
-      dt_txt: date,
-      clouds: { all: cloudiness },
-      main: {
-        humidity,
-        pressure,
-        temp,
-        temp_max: tempMax,
-        temp_min: tempMin,
-        grnd_level: groundLevel,
-        sea_level: seaLevel,
-      },
-      weather: [{
-        main: weatherCondition,
-        description: weatherDescription,
-        icon: weatherIcon,
-      }],
-      wind: {
-        deg: windDirection,
-        speed: windSpeed,
-      },
-    } = data;
+const getWeatherData = async location => {
+  const { data, ok } = await fetchWeatherData(location);
+  if (!ok) {
+    const { message } = data;
+    return { message };
+  }
 
-    return {
-      date,
-      cloudiness,
-      humidity,
-      pressure,
-      temp,
-      tempMax,
-      tempMin,
-      groundLevel,
-      seaLevel,
-      weatherCondition,
-      weatherDescription,
-      weatherIcon,
-      windDirection,
-      windSpeed,
-    };
-  };
+  const { list = [], city: { name: cityName } } = data;
 
   const filtered = [];
 
@@ -54,12 +26,14 @@ const getWeatherData = async (location) => {
   for (let index = 0; index < list.length; index += 1) {
     const listDay = (new Date(list[index].dt_txt)).getDay();
     if (listDay === day) {
-      filtered.push(getForcastData(list[index]));
+      const formatForecastData = formatData(list[index]);
+      filtered.push(formatForecastData);
       day += 1;
       day %= 7;
     }
     if (filtered.length === 5) { break; }
   }
+
   return { forecasts: filtered, cityName };
 };
 
