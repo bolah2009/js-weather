114edadd127436ca5d8e1ca6f5e534d39e08ebfb
Bola Ahmed Buari <bola_ahmed2002@yahoo.com>
2019-10-20 16:32:48 +0100

    Format data and add unit conversions (#4)
    -  Format data unit using  `%`, `°F`, `°C`, `mph`, `km/h`, etc accordingly and add unit conversions from metric to imperial and vice versa
    - Implement two todo features in #2 
    
    TODO: 
     - [ ] Style background to change according to weather conditions
     - [ ] Improve city search using Google Places API to generate search suggestions
     - [ ] Add current location functionality using geolocation API
    
    Live Preview: Please use link generated by `netlify/bolah-js-weather/deploy-preview` below
    To use on development environment, please, configure  [`dotenv`](https://github.com/motdotla/dotenv) using a `.env` file content of this format :point_down:
    
    ```env
    WEATHER_API=<KEY>
    ```
    
    The placeholder of the search input shows the format it accepts which is a city name `city`. e.g. `Lagos`, `London`, `Abuja` and it's not case sensitive. See the demo below.
    
    Screenshot
    ![weather_app2](https://user-images.githubusercontent.com/36057474/67161873-9d1a6300-f356-11e9-9a77-6975f7613db6.gif)

---
+++ src/modules/helpers/formatUnits.js
@@ -0,0 +1,115 @@
+import formatDate from './date';
+
+const formatData = data => {
+  const getForecastData = rawData => {
+    const {
+      dt_txt: date,
+      clouds: { all: cloudiness },
+      main: {
+        humidity,
+        pressure,
+        temp,
+        temp_max: tempMax,
+        temp_min: tempMin,
+        grnd_level: groundLevel,
+        sea_level: seaLevel,
+      },
+      weather: [{
+        main: weatherCondition,
+        description: weatherDescription,
+        icon: weatherIcon,
+      }],
+      wind: {
+        deg: windDirection,
+        speed: windSpeed,
+      },
+    } = rawData;
+
+    return {
+      date,
+      cloudiness,
+      humidity,
+      pressure,
+      temp,
+      tempMax,
+      tempMin,
+      groundLevel,
+      seaLevel,
+      weatherCondition,
+      weatherDescription,
+      weatherIcon,
+      windDirection,
+      windSpeed,
+    };
+  };
+
+  const formatDataDate = ({ date }, type = 'main') => {
+    if (type === 'main') {
+      return formatDate().toFullDayWithTime(date);
+    }
+    return formatDate().toShortDay(date);
+  };
+
+  const formatUnit = (param, paramUnit, obj = {}) => {
+    const value = obj[`${param}`];
+    return `${value.toFixed(0)} ${paramUnit}`;
+  };
+
+  const convertCelsuisToFahrenheit = value => (((value * 9) / 5) + 32);
+  const convertMeterPerSecToMilesPerHour = value => (value * 2.237);
+  const convertMeterPerSecToKiloMeterPerHour = value => (value * 3.6);
+  const convertUnit = (converter, param, paramUnit, obj = {}) => {
+    const value = obj[`${param}`];
+    const convertValue = converter(value);
+    return `${convertValue.toFixed(0)} ${paramUnit}`;
+  };
+
+  const formatMetricUnits = obj => {
+    const metricUnits = {
+      temp: formatUnit('temp', '°C', obj),
+      tempMax: formatUnit('tempMax', '°C', obj),
+      tempMin: formatUnit('tempMin', '°C', obj),
+      windSpeed: convertUnit(convertMeterPerSecToKiloMeterPerHour, 'windSpeed', 'km/h', obj),
+    };
+    return metricUnits;
+  };
+
+  const formatImpericalUnits = obj => {
+    const impericalUnits = {
+      temp: convertUnit(convertCelsuisToFahrenheit, 'temp', '°F', obj),
+      tempMax: convertUnit(convertCelsuisToFahrenheit, 'tempMax', '°F', obj),
+      tempMin: convertUnit(convertCelsuisToFahrenheit, 'tempMin', '°F', obj),
+      windSpeed: convertUnit(convertMeterPerSecToMilesPerHour, 'windSpeed', 'mph', obj),
+    };
+    return impericalUnits;
+  };
+
+  const formatAllParameters = rawData => {
+    const cleanData = getForecastData(rawData);
+    const metricUnits = formatMetricUnits(cleanData);
+    const impericalUnits = formatImpericalUnits(cleanData);
+
+    const otherUnits = {
+      date: formatDataDate(cleanData, 'main'),
+      forecastDate: formatDataDate(cleanData, 'forecast'),
+      cloudiness: formatUnit('cloudiness', '%', cleanData),
+      humidity: formatUnit('humidity', '%', cleanData),
+      pressure: formatUnit('pressure', 'hPa', cleanData),
+      groundLevel: formatUnit('groundLevel', 'hPa', cleanData),
+      seaLevel: formatUnit('seaLevel', 'hPa', cleanData),
+      windDirection: formatUnit('windDirection', '°', cleanData),
+      weatherCondition: cleanData.weatherCondition,
+      weatherDescription: cleanData.weatherDescription,
+      weatherIcon: cleanData.weatherIcon,
+    };
+
+    return {
+      metric: { ...metricUnits, ...otherUnits },
+      imperical: { ...impericalUnits, ...otherUnits },
+    };
+  };
+
+  return formatAllParameters(data);
+};
+
+export default formatData;
